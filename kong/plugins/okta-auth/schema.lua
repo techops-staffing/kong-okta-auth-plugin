b376b51ec58e3d18d5aec5d3bbe7008ea2f880a5
Ana Cardoso <acardoso@thoughtworks.com>
2017-10-13 15:30:50 -0300

    add plugin config params valitadion

--- kong/plugins/okta-auth/schema.lua
+++ kong/plugins/okta-auth/schema.lua
@@ -1,3 +1,5 @@
+local errors = require "kong.dao.errors"
+local okta_api = require "kong.plugins.okta-auth.okta_api"
 return {
   no_consumer = false,
   fields = {
@@ -18,5 +20,23 @@ return {
       default = "v1",
       required = false,
     }
-  }
+  },
+  self_check = function(schema, conf, dao, is_updating)
+    response_body = okta_api.introspect(
+      conf.authorization_server,
+      conf.api_version,
+      conf.client_id,
+      conf.client_secret,
+      "token"
+    )
+
+    if not response_body then
+      return false, errors.schema(
+        "Could not access authorization server ("..
+        conf.authorization_server..
+        ") with the specified configuration"
+      )
+    end
+    return true
+  end
 }
