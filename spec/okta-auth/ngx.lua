fd52f238370cb39c26a6b02b31e7c49d9d018862
Lucas Cunha <luccmir@gmail.com>
2017-10-20 14:54:35 -0200

    Update ngx (test fixture)

--- spec/okta-auth/ngx.lua
+++ spec/okta-auth/ngx.lua
@@ -1,60 +1,17 @@
 -- test fixtures
 return {
   encode_base64 = function(str)
-    return string.format("base64_%s", str)
   end,
-  req = {
-    start_time = function() return 1432844571.623 end,
-    get_method = function() return "GET" end,
-    http_version = function() return 1.1 end,
-    raw_header = function ()
-      return "GET /request/path HTTP/1.1\r\n"..
-             "Host: example.com\r\n"..
-             "Accept: application/json\r\n"..
-             "Accept: application/x-www-form-urlencoded\r\n\r\n"
-    end,
-    get_headers = function()
-      return {
-        accept = {"application/json", "application/x-www-form-urlencoded"},
-        host = "example.com"
-      }
-    end,
-    get_uri_args = function()
-      return {
-        hello = "world",
-        foobar = "baz"
-      }
-    end,
-  },
-  resp = {
-    get_headers = function()
-      return {
-        connection = "close",
-        ["content-type"] = {"application/json", "application/x-www-form-urlencoded"},
-        ["content-length"] = "934"
-      }
-    end
-  },
-
+  req = {},
+  resp = {},
   now = function()
-    return os.time() * 1000  -- adding ngx.time()'s ms resolution
   end,
   log = function(...)
-    local t = {...}
-    table.remove(t, 1)
-    return t
   end,
   sleep = function(t)
-
   end,
-  timer = {
-    at = function() end
-  },
-
-  -- lua-resty-http stubs
-  socket = {
-    tcp = function() end
-  },
+  timer = {},
+  socket = {},
   re = {},
   config = {
     ngx_lua_version = ""
