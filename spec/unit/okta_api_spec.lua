f7d31a8a950b42f6351d4e61e682796cb913ca01
haoliu <haoliu@thoughtworks.com>
2019-02-26 10:42:34 +0800

    [TMA-630][haoliu & llr] Complete unit test for okta-api

--- spec/unit/okta_api_spec.lua
+++ spec/unit/okta_api_spec.lua
@@ -1,5 +1,6 @@
 local okta_api = require "kong.plugins.okta-auth.okta_api"
 local https = require "ssl.https"
+local singletons = require "kong.singletons"
 
 describe("Okta API", function()
   it("returns response if introspect API request status code is 200", function()
@@ -22,6 +23,39 @@ describe("Okta API", function()
     )
 
     assert.is_nil(response)
+
+    https.request:revert()
+  end)
+
+  it("returns oidc_label if get_oidc_label by cache:probe success", function()
+    stub(singletons, "cache").returns("cache")
+    stub(singletons.cache, "probe").returns("ttl", nil, "value")
+
+    local oidc_label = okta_api.get_oidc_label("oidc_id")
+
+    assert.is_not_nil(oidc_label)
+
+  end)
+
+  it("returns oidc_label if get_oidc_label by cache:get success", function()
+    stub(singletons, "cache").returns("cache")
+    stub(singletons.cache, "probe").returns(nil, nil, nil)
+    stub(singletons.cache, "get").returns("oidc_label", nil)
+
+    local oidc_label = okta_api.get_oidc_label("oidc_id")
+
+    assert.is_not_nil(oidc_label)
+
+    https.request:revert()
+  end)
+
+  it("returns oidc_label if get_oidc_label by get_oidc_label send request", function()
+    stub(singletons, "cache").returns(nil)
+    stub(https, "request").returns("oidc_label", 200, "headers", "200 ")
+
+    local oidc_label = okta_api.get_oidc_label("oidc_id")
+
+    assert.is_not_nil(oidc_label)
 
     https.request:revert()
   end)
